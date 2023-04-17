9f9b608bd8126ebb6952ab95f98ffbfc6a66e978
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-17 16:28:15 +0200

    Add listOfCommand

--- src/Runner.java
+++ src/Runner.java
@@ -1,25 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Runner {
 
   enum Command {
-    NEW, // закрытие чека должно открыть следующий
-    ADD, // добавить товар (строку) в чек
-    REPORT, // вывести отчёт
-    EXIT, // выход из программы
+    VOCABULARY,
+    TRAINING,
+    REPORT,
+    EXIT,
   }
 
-  private static final Map<Command, String> commands = new HashMap<>();
+  private static List<Map<Integer, Command>> listOfCommand(){
+    List<Map<Integer, Command>> menu = new ArrayList<>();
 
-  static { // так можно задать статический константный словарь (Map)
-    commands.put(Command.NEW, "Начать новый чек");
-    commands.put(Command.ADD, "Добавить позицию в чек");
-    commands.put(Command.REPORT, "Вывести отчёт");
-    commands.put(Command.EXIT, "Выход");
+    Map<Integer, Command> firstCommand = new HashMap<>();
+    firstCommand.put(1,Command.VOCABULARY);
+
+    Map<Integer, Command> secondCommand = new HashMap<>();
+    secondCommand.put(2,Command.TRAINING);
+
+    Map<Integer, Command> thirdCommand = new HashMap<>();
+    thirdCommand.put(3,Command.REPORT);
+
+    Map<Integer, Command> exitCommand = new HashMap<>();
+    exitCommand.put(3,Command.EXIT);
+
+    menu.add(firstCommand);
+    menu.add(secondCommand);
+    menu.add(thirdCommand);
+    menu.add(exitCommand);
+
+    return menu;
   }
 
 //  public static void main(String[] args) throws IOException {
@@ -40,34 +56,34 @@ public class Runner {
 //    System.out.println("До свидания!");
 //  }
 
-  public static void printMenu() {
-    System.out.println(); // пустая строка для красоты
-    System.out.println("Список команд:");
-    for (Command command : commands.keySet()) {
-      System.out.println("- " + command + ": " + commands.get(command));
-    }
-  }
+//  public static void printMenu() {
+//    System.out.println(); // пустая строка для красоты
+//    System.out.println("Список команд:");
+//    for (Command command : commands.keySet()) {
+//      System.out.println("- " + command + ": " + commands.get(command));
+//    }
+//  }
 
-  public static Command readCommand() throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    printMenu();
-    System.out.println(); // пустая строка для красоты
-    System.out.print("Введите команду: ");
-    String command = br.readLine().toUpperCase();
-
-    Command result = null;
-    while (result == null) { // пока команда не установлена
-      try {
-        result = Command.valueOf(command); // пытаемся установить команду
-      } catch (IllegalArgumentException e) {
-        System.out.println("Некорректная команда: " + command);
-        System.out.print("Введите корректную команду: ");
-        command = br.readLine().toUpperCase();
-      }
-    }
-
-    System.out.println(); // пустая строка для красоты
-    return result;
-  }
+//  public static Command readCommand() throws IOException {
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//    printMenu();
+//    System.out.println(); // пустая строка для красоты
+//    System.out.print("Введите команду: ");
+//    String command = br.readLine().toUpperCase();
+//
+//    Command result = null;
+//    while (result == null) { // пока команда не установлена
+//      try {
+//        result = Command.valueOf(command); // пытаемся установить команду
+//      } catch (IllegalArgumentException e) {
+//        System.out.println("Некорректная команда: " + command);
+//        System.out.print("Введите корректную команду: ");
+//        command = br.readLine().toUpperCase();
+//      }
+//    }
+//
+//    System.out.println(); // пустая строка для красоты
+//    return result;
+//  }
 }
