4d7fd37040787da4197b319cc935e6a9ffa37162
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-17 19:25:03 +0200

    add class Dictionary and Wort

---
+++ src/Dictionary.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+
+  public static List<Wort> getList(){
+    List<Wort> sortVocable = new ArrayList<>();
+    System.out.println("HERE SHOULD BE LIST OF WORT");
+    return sortVocable;
+  }
+
+}
