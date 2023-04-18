77c2898be23199be29f6d9661e243239a66f7b19
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-18 06:18:53 +0200

    create Word class

--- src/Word.java
+++ src/Word.java
@@ -1,3 +1,36 @@
 public class Word {
 
+  private String englishWord;
+  private String deutschWord;
+
+  private int numberOfMentions = 0;
+
+  public Word(String englishWord, String deutschWord) {
+    this.englishWord = englishWord;
+    this.deutschWord = deutschWord;
+  }
+
+  public String getDeutschWord() {
+    return deutschWord;
+  }
+
+  public String getEnglishWord() {
+    return englishWord;
+  }
+
+  public int getNumberOfMentions() {
+    return numberOfMentions;
+  }
+
+  public void setDeutschWord(String deutschWord) {
+    this.deutschWord = deutschWord;
+  }
+
+  public void setEnglishWord(String englishWord) {
+    this.englishWord = englishWord;
+  }
+
+  public void setNumberOfMentions(int numberOfMentions) {
+    this.numberOfMentions = numberOfMentions;
+  }
 }
